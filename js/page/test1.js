fbaaeceddd77b80726a320809d90101e7f57eaab
Xiang Gu <c0710204@gmail.com>
2012-10-05 23:23:39 +0800

    10/05/12 23:22 day-end
    完成list加载,采用替换#xxx#的模式进行加载

--- js/page/test1.js
+++ js/page/test1.js
@@ -1,9 +1,11 @@
 
 function work()
 {
-	var data='[{"demo":"row1demo",	"rows":[{"attr1":"a"},{"attr1":"b"}]},{"demo":"row1demo",	"rows":[{"attr1":"a"},{"attr1":"b"}]}]';
- var data = eval('(' + data + ')');
+//	var data='[{"demo":"row1demo",	"rows":[{"attr1":"a"},{"attr1":"b"}]},{"demo":"row1demo",	"rows":[{"attr1":"a"},{"attr1":"b"}]}]';
+	 data=$.getJSON("/js/data/test1.json",function(data){
+	
 for(var squrow in data){
-loadsqu('squ1demo',data[squrow],'squ1demo');}
+loadsqu('squ1demo',data[squrow],'squ1demo');}});
+// var data = eval('(' + data + ')');
 }
 setTimeout(work(),1000);
